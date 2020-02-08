8fb4a8b7676888de2539c8910df19b8371e927c5
dbacall <dbacall@hotmail.co.uk>
2020-02-08 13:58:20 +0000

    chore: enable circleci to use api key

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   ScrollView,
@@ -7,7 +7,8 @@ import {
   Button,
   ActivityIndicator,
 } from 'react-native';
-import {API_KEY} from '../utils/RapidApiSpoonacularApiKey';
+import { SPOONACULAR_API_KEY } from '../utils/RapidApiSpoonacularApiKey';
+
 export default class Ingredients extends Component {
   constructor(props) {
     super(props);
@@ -15,36 +16,63 @@ export default class Ingredients extends Component {
       isLoading: true,
       ingredientsData: [],
       ingredients: [],
+      apiKey: SPOONACULAR_API_KEY,
     };
   }
+
+  
+
   componentDidMount() {
-    if (this.props.ingredients?.length) {
-      let transcript = this.props.ingredients;
-      let parameter = this.deleteWordDuplicates(transcript).join(' ');
-      let body = 'text=' + parameter;
-      this.fetchIngredients(body);
-    }
+    let transcript = this.props.ingredients
+    let parameter = this.deleteWordDuplicates(transcript).join(' ');
+    let body = 'text=' + parameter;
+    this.fetchIngredients(body)
   }
   deleteWordDuplicates(transcript) {
     let wordsArray = transcript.join(' ').split(' ');
     return wordsArray.filter((a, b) => wordsArray.indexOf(a) === b);
   }
-
+  fetchIngredients(body, url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/detect') {
+    if (SPOONACULAR_API_KEY == null) {
+      this.state.apiKey = System.getenv("SPOONACULAR_API_KEY")
+    } else {
+      this.state.apiKey = SPOONACULAR_API_KEY
+    }
+    fetch(url, {
+    	method: 'POST',
+    	headers: {
+    		'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+    		'x-rapidapi-key': this.state.apiKey,
+        Accept: 'application/json',
+    		'content-type': 'application/x-www-form-urlencoded',
+    	},
+    	body: body,
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        ingredientsData: responseJson.annotations,
+        isLoading: false,
+      })
+    })
+    .catch(err => {
+    	console.log(err);
+    });
+  }
   render() {
     if (this.state.isLoading) {
       return (
         <View>
-          <ActivityIndicator />
+          <ActivityIndicator/>
         </View>
-      );
+      )
     } else {
-      return (
-        <ScrollView>
-          {this.state.ingredientsData.map((val, key) => (
-            <Text> {val.annotation} </Text>
-          ))}
-        </ScrollView>
-      );
+        return (
+          <ScrollView>
+          { this.state.ingredientsData.map((val, key) =>
+          <Text> {val["annotation"]} </Text> )}
+      </ScrollView>
+    )
+      }
     }
   }
-}
