fe3aebbed04cb9062365af92439ffb33597dc69b
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-02-12 11:42:27 +0000

    style: completed Recipe page

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {SPOONACULAR_API_KEY} from '../utils/RapidApiSpoonacularApiKey';
-import {ScrollView, Text, View, Image, ActivityIndicator} from 'react-native';
-
+import {StyleSheet, ScrollView, TouchableOpacity, Text, View, Image, ActivityIndicator} from 'react-native';
 export default class Recipe extends Component {
   constructor(props) {
     super(props);
@@ -13,56 +12,327 @@ export default class Recipe extends Component {
     let instructions = this.props.individualRecipeData.analyzedInstructions;
 
     return (
-      <ScrollView>
-        <View>
-          <Image
-            source={{uri: recipe.image}}
-            style={{width: 400, height: 400}}
-          />
-          <Text>
-            <Text>{recipe.title} </Text>
-            <Text>
-              {' '}
-              {'\n'}
-              {recipe.readyInMinutes} minutes
-            </Text>
-            <Text>
-              {' '}
-              {'\n'}
-              {recipe.servings} servings
-            </Text>
-            {ingredientsList.map((j, k) => (
-              <Text>
-                {' '}
-                {'\n'}
-                {ingredientsList[k].original}{' '}
-              </Text>
-            ))}
+      <View
+				style={styles.viewView}>
+				<ScrollView
+					contentContainerStyle={styles.viewScrollView} >
+					<View
+						pointerEvents="box-none"
+						style={{
+							alignSelf: "stretch",
+							height: 354,
+						}}>
+						<View
+							pointerEvents="box-none"
+							style={{
+								position: "absolute",
+								left: 0,
+								right: 0,
+								top: 0,
+								height: 291,
+							}}>
+							<Image
+								source={{uri: recipe.image}}
+								style={styles.recipeImage}/>
+							<Text
+								style={styles.recipeTitle}>{recipe.title}</Text>
+						</View>
+						<TouchableOpacity
+							onPress={this.onGroup6Pressed}
+							style={styles.group6Button}>
+							<Image
+								source={require("./../../assets/images/group-6.png")}
+								style={styles.group6ButtonImage}/>
+						</TouchableOpacity>
+						<View
+							style={styles.viewTwoView}>
+							<View
+								style={styles.rectangle30View}/>
+							<Text
+								style={styles.servingsText}>  {recipe.servings}{"\n"}Servings</Text>
+							<Image
+								source={require("./../../assets/images/user.png")}
+								style={styles.userImage}/>
+						</View>
+						<View
+							style={styles.viewThreeView}>
+							<View
+								style={styles.rectangle29View}/>
+							<Text
+								style={styles.minutesText}>  {recipe.readyInMinutes}{"\n"}Minutes</Text>
+							<Image
+								source={require("./../../assets/images/stopwatch.png")}
+								style={styles.stopwatchImage}/>
+						</View>
+					</View>
+					<View
+						style={styles.viewFourView}>
+						<Text
+							style={styles.ingredientsText}>Ingredients</Text>
+						<Image
+							source={require("./../../assets/images/path-12.png")}
+							style={styles.path12Image}/>
+					</View>
+					<Text style={styles.ingredientText}>
+          {ingredientsList.map((j, k) => (
+            <Text>{ingredientsList[k].original}{"\n"}</Text>
+          ))}
           </Text>
-        </View>
-
-        <View>
-          {instructions.map((item, key) => (
-            <Text>
+					<View
+						pointerEvents="box-none"
+						style={{
+							flex: 1,
+							width: 289,
+							marginRight: 35,
+							marginTop: 16,
+							marginBottom: 110,
+						}}>
+						<View
+							style={styles.viewFiveView}>
+							<Text
+								style={styles.methodText}>Method</Text>
+							<Image
+								source={require("./../../assets/images/path-12.png")}
+								style={styles.path13Image}/>
+						</View>
+            {instructions.map((item, key) => (
+						<Text style={styles.stepText}>
               {instructions[key].name.length > 0 && (
                 <Text>{instructions[key].name}</Text>
               )}
               {instructions[key].steps.map((i, k) => (
-                <Text>
-                  <Text>
-                    {'\n'}
-                    {instructions[key].steps[k].number}
-                  </Text>
-                  <Text>
-                    {'\n'}
-                    {instructions[key].steps[k].step}
-                  </Text>
-                </Text>
+              <Text>{instructions[key].steps[k].number}.  {instructions[key].steps[k].step}{"\n"}{"\n"}</Text>
               ))}
             </Text>
-          ))}
-        </View>
-      </ScrollView>
+            ))}
+					</View>
+				</ScrollView>
+			</View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+	viewView: {
+		backgroundColor: "white",
+		flex: 1,
+	},
+	viewScrollView: {
+		backgroundColor: "transparent",
+		alignItems: "flex-end",
+	},
+	recipeImage: {
+		backgroundColor: "transparent",
+		resizeMode: "cover",
+		width: null,
+		height: 193,
+	},
+	recipeTitle: {
+		color: "black",
+		fontFamily: "AdobeArabicRegular",
+		fontSize: 38,
+		fontStyle: "normal",
+		fontWeight: "normal",
+		textAlign: "left",
+		backgroundColor: "transparent",
+		alignSelf: "center",
+		width: 311,
+		marginTop: 16,
+	},
+	group6ButtonImage: {
+		resizeMode: "contain",
+	},
+	group6Button: {
+		backgroundColor: "transparent",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 0,
+		position: "absolute",
+		left: 13,
+		width: 33,
+		top: 10,
+		height: 32,
+	},
+	group6ButtonText: {
+		color: "black",
+		fontSize: 12,
+		fontStyle: "normal",
+		fontWeight: "normal",
+		textAlign: "left",
+	},
+	viewTwoView: {
+		backgroundColor: "transparent",
+		position: "absolute",
+		left: 13,
+		width: 289,
+		top: 10,
+		height: 344,
+	},
+	rectangle30View: {
+		backgroundColor: "rgb(211, 199, 193)",
+		opacity: 0.76,
+		borderRadius: 11,
+		position: "absolute",
+		right: 0,
+		width: 110,
+		top: 284,
+		height: 60,
+	},
+	servingsText: {
+		color: "black",
+		fontFamily: "AdobeArabicRegular",
+		fontSize: 24,
+		fontStyle: "normal",
+		fontWeight: "normal",
+		textAlign: "center",
+		lineHeight: 19,
+		paddingTop: 8,
+		backgroundColor: "transparent",
+		position: "absolute",
+		right: 10,
+		width: 90,
+		top: 293,
+	},
+	userImage: {
+		resizeMode: "center",
+		backgroundColor: "transparent",
+		position: "absolute",
+		right: 59,
+		width: 14,
+		top: 298,
+		height: 14,
+	},
+	viewThreeView: {
+		backgroundColor: "transparent",
+		position: "absolute",
+		left: 56,
+		width: 110,
+		top: 294,
+		height: 60,
+	},
+	rectangle29View: {
+		backgroundColor: "rgb(211, 199, 193)",
+		opacity: 0.76,
+		borderRadius: 11,
+		position: "absolute",
+		left: 0,
+		width: 110,
+		top: 0,
+		height: 60,
+	},
+	minutesText: {
+		backgroundColor: "transparent",
+		color: "black",
+		fontFamily: "AdobeArabicRegular",
+		fontSize: 24,
+		fontStyle: "normal",
+		fontWeight: "normal",
+		textAlign: "center",
+		lineHeight: 19,
+		paddingTop: 8,
+		position: "absolute",
+		left: 12,
+		width: 90,
+		top: 9,
+	},
+	stopwatchImage: {
+		backgroundColor: "transparent",
+		resizeMode: "center",
+		position: "absolute",
+		left: 34,
+		width: 15,
+		top: 14,
+		height: 15,
+	},
+	viewFourView: {
+		backgroundColor: "transparent",
+		alignSelf: "center",
+		width: 289,
+		height: 33,
+		marginTop: 26,
+	},
+	ingredientsText: {
+		backgroundColor: "transparent",
+		color: "black",
+		fontFamily: "AdobeArabicRegular",
+		fontSize: 30,
+		fontStyle: "normal",
+		fontWeight: "normal",
+		textAlign: "left",
+		position: "absolute",
+		left: 6,
+		width: 166,
+		top: 0,
+	},
+	path12Image: {
+		backgroundColor: "transparent",
+		resizeMode: "cover",
+		position: "absolute",
+		alignSelf: "center",
+		width: 289,
+		top: 30,
+		height: 3,
+	},
+	ingredientText: {
+		color: "rgb(62, 60, 60)",
+		fontFamily: "AdobeArabicRegular",
+		fontSize: 24,
+		fontStyle: "normal",
+		fontWeight: "normal",
+		textAlign: "left",
+		lineHeight: 20,
+		paddingTop: 7,
+		backgroundColor: "transparent",
+		width: 281,
+		// height: 29,
+		marginRight: 35,
+		marginTop: 3,
+	},
+	viewFiveView: {
+		backgroundColor: "transparent",
+		// position: "absolute",
+		alignSelf: "center",
+		width: 289,
+		top: 0,
+		height: 33,
+	},
+	methodText: {
+		backgroundColor: "transparent",
+		color: "black",
+		fontFamily: "AdobeArabicRegular",
+		fontSize: 30,
+		fontStyle: "normal",
+		fontWeight: "normal",
+		textAlign: "left",
+		position: "absolute",
+		left: 6,
+		width: 166,
+		top: 0,
+	},
+	path13Image: {
+		backgroundColor: "transparent",
+		resizeMode: "cover",
+		position: "absolute",
+		alignSelf: "center",
+		width: 289,
+		top: 30,
+		height: 3,
+	},
+	stepText: {
+		backgroundColor: "transparent",
+		color: "rgb(62, 60, 60)",
+		fontFamily: "AdobeArabicRegular",
+		fontSize: 24,
+		fontStyle: "normal",
+		fontWeight: "normal",
+		textAlign: "left",
+		lineHeight: 20,
+		paddingTop: 7,
+		// position: "absolute",
+		right: 2,
+		width: 281,
+		bottom: 0,
+		// height: 68,
+	},
+})
